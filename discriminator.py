94bd98090f175cd2f6fc629bb5bd76c656bf2e12
LamUong <lam.uong2@mail.mcgill.ca>
2018-01-25 21:47:45 -0800

    update

--- discriminator.py
+++ discriminator.py
@@ -2,67 +2,72 @@
 The discriminator architecture
 Since we are not using Batch Normalization for discriminator set Bias = True
 '''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 class residual_block(nn.Module):
     def __init__(self, in_chanel, kernel_size, out_chanel, stride, dropRate=0.0):
         super(residual_block, self).__init__()
-        self.conv1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
+        self.conv_1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
                                padding=1, bias=True)
-        self.leaky_relu1 = nn.LeakyReLU()
-        self.conv2 = nn.Conv2d(out_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
+        self.leaky_relu_1 = nn.LeakyReLU()
+        self.conv_2 = nn.Conv2d(out_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
                                padding=1, bias=True)
-        self.leaky_relu2 = nn.LeakyReLU()
+        self.leaky_relu_2 = nn.LeakyReLU()
    
     def forward(self, x):
-        o = self.conv1(x)
-        o = self.leaky_relu1(o)
-        o = self.conv2(o)
-        o = o+x
-        o = self.leaky_relu2(o)
+        r = x
+        o = self.conv_1(x)
+        o = self.leaky_relu_1(o)
+        o = self.conv_2(o)
+        o += r
+        o = self.leaky_relu_2(o)
         return o
 
 class down_sample_block(nn.Module):
     def __init__(self, in_chanel, kernel_size, out_chanel, stride, dropRate=0.0):
         super(down_sample_block, self).__init__()
-        self.conv1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
+        self.conv_1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
                                padding=1, bias=True)
-        self.leaky_relu1 = nn.LeakyReLU()
+        self.leaky_relu_1 = nn.LeakyReLU()
     
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.leaky_relu1(x)
-        return x
+        o = self.conv_1(x)
+        o = self.leaky_relu_1(o)
+        return o
 
 class repeated_block(nn.Module)
     def __init__(self, in_chanel, out_chanel, down_sample_kernel, dropRate=0.0):
         super(repeated_block, self).__init__()
-        self.d = down_sample_block(in_chanel,down_sample_kernel,out_chanel,2)
-        self.r1 = residual_block(out_chanel,3,out_chanel,1)
-        self.r2 = residual_block(out_chanel,3,out_chanel,1)
+        self.down_sample_1 = down_sample_block(in_chanel,down_sample_kernel,out_chanel,2)
+        self.residual_1 = residual_block(out_chanel,3,out_chanel,1)
+        self.residual_2 = residual_block(out_chanel,3,out_chanel,1)
     
     def forward(self, x):
-        x = self.d(x)
-        x = self.r1(x)
-        x = self.r2(x)
-        return x
+        o = self.down_sample_1(x)
+        o = self.residual_1(o)
+        o = self.residual_2(o)
+        return o
 
 class discriminator(nn.Module):
-	def __init__(self, tags=19):
+	def __init__(self, tags=34):
         super(discriminator, self).__init__()
-        self.r1 = repeated_block(3,32,4)
-        self.r2 = repeated_block(32,64,4)
-        self.r3 = repeated_block(64,128,4)
-        self.r4 = repeated_block(128,256,3)
-        self.r5 = repeated_block(256,512,3)
-        self.d1 = down_sample_block(512,3,1024,2)
+        self.r_1 = repeated_block(3,32,4)
+        self.r_2 = repeated_block(32,64,4)
+        self.r_3 = repeated_block(64,128,4)
+        self.r_4 = repeated_block(128,256,3)
+        self.r_5 = repeated_block(256,512,3)
+        self.d_1 = down_sample_block(512,3,1024,2)
         self.dense_1 = nn.Linear(2*2*1024, 1)
         self.dense_tags = nn.Linear(2*2*1024, tags)
         
     def forward(self, x):
-        x = self.r1(x)
-        x = self.r2(x)
-        x = self.r3(x)
-        x = self.r4(x)
-        x = self.r5(x)
-        x = self.d1(x)
-        x = x.view(x.size()[0],-1)
-        return self.dense_1(x),self.dense_tags(x)
+        o = self.r_1(x)
+        o = self.r_2(o)
+        o = self.r_3(o)
+        o = self.r_4(o)
+        o = self.r_5(o)
+        o = self.d_1(o)
+        o = o.view(o.size()[0],-1)
+        return self.dense_1(o),self.dense_tags(o)
--- generator.py
+++ generator.py
@@ -2,38 +2,83 @@
 The generator architecture
 Since we are  using Batch Normalization for discriminator set Bias = False for prev layer
 '''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 class residual_block(nn.Module):
     def __init__(self, in_chanel, kernel_size, out_chanel, stride, dropRate=0.0):
         super(residual_block, self).__init__()
-        self.conv1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
+        self.conv_1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
                                padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_chanel)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(out_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
+        self.bn_1 = nn.BatchNorm2d(out_chanel)
+        self.relu_1 = nn.ReLU()
+        self.conv_2 = nn.Conv2d(out_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
                                padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_chanel)
+        self.bn_2 = nn.BatchNorm2d(out_chanel)
    
     def forward(self, x):
-        o = self.conv1(x)
-        o = self.bn1(o)
-        o = self.relu1(o)
-        o = self.conv2(o)
-        o = self.bn2(o)
-        o = o+x
+        r = x
+        o = self.conv_1(x)
+        o = self.bn_1(o)
+        o = self.relu_1(o)
+        o = self.conv_2(o)
+        o = self.bn_2(o)
+        o += r
         return y
 
 class subpixel_block(nn.Module):
     def __init__(self, in_chanel, kernel_size, out_chanel, stride, dropRate=0.0):
         super(residual_block, self).__init__()
-        self.conv1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
+        self.conv_1 = nn.Conv2d(in_chanel, out_chanel, kernel_size=kernel_size, stride=stride,
                                padding=1, bias=False)
         self.pixel_shuffler = nn.PixelShuffle(2) 
-        self.bn1 = nn.BatchNorm2d(in_chanel)
-        self.relu1 = nn.ReLU()
+        self.bn_1 = nn.BatchNorm2d(in_chanel)
+        self.relu_1 = nn.ReLU()
    
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pixel_shuffler(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        return x
+        o = self.conv_1(x)
+        o = self.pixel_shuffler(o)
+        o = self.bn_1(o)
+        o = self.relu_1(o)
+        return o
+
+class generator(nn.Module):
+    def __init__(self, tags=34):
+        super(generator, self).__init__()
+        self.dense_1 = nn.Linear(128+tags, 16*16*64)
+        self.bn_1 = nn.BatchNorm2d(64)
+        self.relu_1 = nn.ReLU()
+        self.residual = self.get_residual_blocks(16)
+        self.bn_2 = nn.BatchNorm2d(64)
+        self.relu_2 = nn.ReLU()
+        self.subpixel = self.get_subpixel_blocks(3)
+        self.conv_1 = nn.Conv2d(64, 3, kernel_size=9, stride=1,padding=4, bias=True)
+        self.tanh_1 = nn.Tanh()
+
+
+    def get_residual_blocks(self,num_blocks):
+        layers = []
+        for i in range(num_blocks):
+            layers.append(residual_block(64,3,64,1))
+        return nn.Sequential(*layers)
+
+    def get_subpixel_blocks(self,num_blocks):
+        layers = []
+        for i in range(num_blocks):
+            layers.append(subpixel_block(64,3,256,1))
+        return nn.Sequential(*layers)      
+        
+    def forward(self, x):
+        o = self.dense_1(x)
+        o = o.view(-1,64,16,16)
+        o = self.bn_1(o)
+        o = self.relu_1(o)
+        r = o
+        o = self.residual(o)
+        o = self.bn_2(o)
+        o = self.relu_2(o)
+        o += r       
+        o = self.subpixel(o)
+        o = self.conv_1(o)
+        o = self.tanh_1(o)
